9c5df938888245c58f11355af93d787af7c46e9b
ranawaqarahmad <ranawaqarahmad33@gmail.com>
2023-08-22 19:17:17 +0500

    final commit

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,46 @@
+import { Resolver, Query, Mutation, Args, ResolveField, Parent } from '@nestjs/graphql';
+import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
+import { CreateLessonInput } from './lesson.input';
+import { AssignStudentsToLessonInput } from './asssign-students-to-lesson.input';
+import { Lesson } from './lesson.entity';
+import { StudentService } from 'src/student/student.service';
+
+@Resolver((of) => LessonType)
+export class LessonResolver {
+  constructor(
+    private lessonService: LessonService,
+    private studentService: StudentService
+    ) {}
+
+  @Query((returns) => [LessonType])
+  allLessons() {
+    return this.lessonService.getAllLessons();
+  }
+
+  @Query((returns) => LessonType)
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
+  }
+
+  @Mutation((returns) => LessonType)
+  createLesson(
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
+  ) {
+    return this.lessonService.createLesson(createLessonInput);
+  }
+
+  @Mutation((returns) => LessonType)
+  assignStudentsToLesson(
+    @Args('assignStudentsToLessonInput')
+    assignStudentsToLessonInput: AssignStudentsToLessonInput,
+  ) {
+    const { lessonId, studentIds } = assignStudentsToLessonInput;
+    return this.lessonService.assignStudentsToLesson(lessonId, studentIds);
+  }
+
+  @ResolveField()
+  async students(@Parent() lesson: Lesson) {
+    return this.studentService.getManyStudents(lesson.students)
+  }
+}
---
+++ src/student/student.resolver.ts
@@ -0,0 +1,26 @@
+import { Resolver, Query , Mutation , Args } from "@nestjs/graphql";
+import { StudentType } from "./student.type";
+import { StudentService } from "./student.service";
+import { CreateStudentInput } from "./create-student.input";
+
+@Resolver((of) => StudentType)
+export class StudentResolver {
+    constructor(private studentService: StudentService){}
+
+    @Query((returns) => [StudentType])
+    allStudents(){
+        return this.studentService.getAllStudents()
+    }
+
+    @Query((returns) => StudentType)
+    student(@Args('id') id:string) {
+        return this.studentService.getStudent(id)
+    }
+
+    @Mutation((returns) => StudentType)
+    createStudent(
+        @Args('createStudentInput') createStudentInput: CreateStudentInput
+    ) {
+        return this.studentService.createStudent(createStudentInput)
+    }
+}
